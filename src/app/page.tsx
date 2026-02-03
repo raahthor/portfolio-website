f36c2fe96a358a6a4dc47787b38e6c2933379c28
Prashant Rathore <prashantrathore1435@gmail.com>
2026-02-03 19:51:06 +0530

    moved sections into comp/sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ import styles from "./page.module.css";
 
 import NavBar from "@/components/NavBar";
 import SideBar from "@/components/SideBar";
-import HomeSection from "@/components/HomeSection";
-import AboutSection from "@/components/AboutSection";
-import SkillsSection from "@/components/SkillsSection";
-import ProjectsSection from "@/components/ProjectsSection";
-import ContactSection from "@/components/ContactSection";
+import HomeSection from "@/components/sections/HomeSection";
+import AboutSection from "@/components/sections/AboutSection";
+import SkillsSection from "@/components/sections/SkillsSection";
+import ProjectsSection from "@/components/sections/ProjectsSection";
+import ContactSection from "@/components/sections/ContactSection";
 import ParticlesBG from "@/components/ParticlesBg";
 
 import { Analytics } from "@vercel/analytics/react";
